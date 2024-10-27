04b43a4a61f41cd306bdf7a779e79e8af4b856cc
MohamedFathi742 <mofathi742002@gmail.com>
2024-10-27 22:20:15 +0300

    Add project files.

---
+++ numberClassification/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace numberClassification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Number-Classification app
+            Console.WriteLine("welcome to Number-Classification app  ");
+            // take value from user
+            Console.WriteLine(" please enter value ");
+            //read value
+            double value = 0;
+            bool converted = double.TryParse(Console.ReadLine(), out value);
+            // if 
+            if (!converted)
+            {
+                Console.WriteLine(" please enter Correct value ");
+
+                return;
+            }
+            if (value < 0)
+            {
+                Console.WriteLine("the value less than zero ");
+            }
+            else if (value == 0)
+            {
+                Console.WriteLine("the value Equal to zero ");
+            }
+
+            else if (value > 0)
+            {
+                Console.WriteLine("the value Greatest than zero ");
+            }
+
+
+        }
+    }
+}
